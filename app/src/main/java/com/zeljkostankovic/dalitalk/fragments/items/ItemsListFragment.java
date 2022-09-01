cc44afe757966768019c210a47dd3222d6be9747
Zeljko Stankovic <zeljko.stankovic@hftm.ch>
2022-09-01 18:57:47 +0200

    added RecyclerView to list all Items

--- app/src/main/java/com/zeljkostankovic/dalitalk/fragments/items/ItemsListFragment.java
+++ app/src/main/java/com/zeljkostankovic/dalitalk/fragments/items/ItemsListFragment.java
@@ -3,7 +3,10 @@ package com.zeljkostankovic.dalitalk.fragments.items;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +14,15 @@ import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.zeljkostankovic.dalitalk.R;
+import com.zeljkostankovic.dalitalk.adapters.ItemAdapter;
+import com.zeljkostankovic.dalitalk.viewModels.ItemViewModel;
 
 public class ItemsListFragment extends Fragment {
+
+    FloatingActionButton fab;
+    RecyclerView recyclerView;
+    ItemAdapter itemAdapter;
+    ItemViewModel itemViewModel;
 
 
     @Override
@@ -21,13 +31,18 @@ public class ItemsListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_items_list, container, false);
 
-        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fabItemsList);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(v).navigate(R.id.action_itemsListFragment_to_itemsAddFragment);
-            }
+        fab = view.findViewById(R.id.fabItemsList);
+        recyclerView = view.findViewById(R.id.recyclerItemsList);
+        itemAdapter = new ItemAdapter();
+        itemViewModel = new ViewModelProvider(this).get(ItemViewModel.class);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(itemAdapter);
+
+        itemViewModel.getmAllItems().observe( getViewLifecycleOwner(), items -> {
+            itemAdapter.setData(items);
         });
+
+        fab.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_itemsListFragment_to_itemsAddFragment));
 
         return view;
     }
